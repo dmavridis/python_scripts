6d927571623fb664eefa23eb41096077465d1a47
Dimitrios Mavridis <dmavridis@outlook.com>
2016-09-12 17:29:13 +0100

    Update pandas_group_add.py

--- pandas_group_add.py
+++ pandas_group_add.py
@@ -16,3 +16,8 @@ dict_pd.columns = ['year','name','times']
 
 grp_year = dict_pd.groupby('year')
 grp_year = dict_pd.groupby(['name','year'], as_index = False).sum()
+
+food_pd = pd.DataFrame.from_dict(dict1, orient='index')
+food_pd.columns = ['year','name','food']
+
+skata = pd.merge(grp_year,food_pd)
